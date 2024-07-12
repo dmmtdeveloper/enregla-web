be1380c5af1412b897eebbd39c7d32ec56a48cb8
David Martínez <125302548+dmmtdeveloper@users.noreply.github.com>
2024-07-12 10:54:17 -0400

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
+import clsx from "clsx";
 
 const sora = Sora({ subsets: ["latin"] });
 
@@ -18,13 +19,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${sora.className} bg-gray-50 text-gray-950 relative`}>
+        <div
+          className={clsx(
+            "bg-[#7ADB78]",
+            " absolute top-[-6rem]",
+            "-z-10 right-[11rem]",
+            "h-[31.25rem] w-[31.25rem]",
+            "rounded-full blur-[10rem] sm:w-[68.75rem]"
+          )}
+        ></div>
 
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg-left-[-28rem] xl-left-[-15rem] 2xl:left-[]">
-
-        </div>
-        <Navbar/>
+        <div
+          className={clsx(
+            "bg-[#dbd7fb]",
+            "absolute top-[-1rem] -z-10",
+            "left-[-35rem] h-[31.25rem] w-[50rem]",
+            "rounded-full blur-[10rem] sm:w-[68.75rem]",
+            "md:left-[-33rem] lg-left-[-28rem] xl-left-[-15rem] 2xl:left-[]"
+          )}
+        ></div>
+        <Navbar />
         {children}
       </body>
     </html>
