4df26e7901381af304dbbbdcc2475df81db000d4
David Martínez <dmmtapia.ux@gmail.com>
2024-07-26 23:02:35 -0400

    changes

--- components/AppSection.tsx
+++ components/AppSection.tsx
@@ -2,26 +2,30 @@
 import Image from "next/image";
 import React from "react";
 import postImageenregla from "@/public/assets/img/post.png";
-import { useRouter } from "next/navigation";
+
 
 export const AppSection = () => {
-  const router = useRouter();
   return (
     <section id="App" className="flex items-center justify-center flex-col gap-5 px-8 mt-20 scroll-mt-28">
       <h2 className="text-3xl dark:text-white text-gray-900 font-semibold">
         Reformulando el proceso de grabado de patentes en vehículos
       </h2>
 
-      <article className="dark:bg-[#2B263D] bg-gray-200 w-full items-center justify-center flex py-10 gap-2 p-4 rounded-2xl">
-        <div className="flex flex-col gap-2">
-          <p className="dark:text-white text-gray-900 text-3xl ">
+      <article className="dark:bg-[#2B263D] bg-gray-200 w-full items-center justify-center flex flex-col py-10 gap-2 p-4 rounded-2xl">
+        <div className="flex flex-col gap-2 pb-10">
+          <h2 className="dark:text-white text-gray-900 text-3xl">
             Máquina <span className="text-[#7ADB78] font-semibold">POS</span> con app móvil
-          </p>
-          <p className="text-gray-400 ">
+          </h2>
+          <p className=" text-gray-400 ">
             Lectura automática de patente sin necesidad de tipeo, asegurando un proceso de grabado sin errores.
           </p>
         </div>
-        <Image className="w-48 h-auto" src={postImageenregla} alt="appenregla" width={256} height={455} priority />
+        <Image 
+          className="w-full h-auto" 
+          src={postImageenregla} 
+          alt="appenregla" 
+          width={256} 
+          height={455} priority />
       </article>
 
       <article className="dark:bg-[#2B263D] bg-gray-200 w-full items-center justify-center flex flex-col py-10 gap-2 p-4 rounded-2xl mb-28">
@@ -34,9 +38,6 @@ export const AppSection = () => {
           </p>
         </div>
       </article>
-
-      {/* <Link href="contact">contact</Link> */}
-      <span onClick={() => router.push("/contact")}>Contact</span>
     </section>
   );
 };
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,14 +2,13 @@
 import clsx from "clsx";
 import React from "react";
 import { motion } from "framer-motion";
-import { HeroPicture } from "./IconsImages/HeroImage";
 import { Button } from "@nextui-org/button";
 
 export const HeroSection = () => {
   return (
     <section
       id="inicio"
-      className="w-full pb-[2rem] flex items-center text-center justify-center flex-col gap-16 px-8 pt-48 xl:pt-72 hero_background"
+      className="z-[1] relative w-full pb-[2rem] flex items-center text-center justify-center flex-col gap-16 px-8 pt-48 xl:pt-72 hero_background"
     >
       <div className="flex flex-col gap-4">
         <div className="flex flex-col justify-center text-center items-center">
@@ -17,15 +16,16 @@ export const HeroSection = () => {
             initial={{ y: -100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             className={clsx(
-              "text-[38px]",
+              "text-[48px]",
               "md:text-5xl",
               "lg:text-6xl",
               "xl:text-7xl",
               "xl:w-[900px]",
-              "text-gray-900",
+              // "text-gray-900",
+              "drop-shadow-md",
               "text-center",
               "font-bold",
-              "leading-tight px-[30px] md:px-8",
+              "leading-tight md:px-8",
               "md:leading-tight",
               "xl:leading-tight",
 
@@ -42,7 +42,7 @@ export const HeroSection = () => {
           animate={{ y: 0, opacity: 1 }}
           className="flex items-center justify-center flex-col gap-6 "
         >
-          <p className="md:px-28 lg:px-60 text-center text-[16px] text-gray-800 dark:text-[#CAC6DD] leading-7">
+          <p className="md:px-28 lg:px-60 text-center text-[16px] dark:text-[#CAC6DD] leading-7">
             Compromiso en otorgar soluciones integrales que protejan a nuestros clientes y sus vehículos.
           </p>
 
@@ -51,7 +51,6 @@ export const HeroSection = () => {
           </Button>
         </motion.div>
       </div>
-      {/* <HeroPicture /> */}
     </section>
   );
 };
