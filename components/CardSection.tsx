46c4062f49f2c7098dd864855991c66ede942da1
David Martínez <dmmtapia.ux@gmail.com>
2024-07-26 20:06:06 -0400

    changes

--- components/CardSection.tsx
+++ components/CardSection.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Card } from "./card/Card";
 import {motion} from "framer-motion"
+import { Button } from "@nextui-org/button";
 
 export const CardSection = () => {
   return (
@@ -9,6 +10,7 @@ export const CardSection = () => {
     initial={{ y: -100, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
     className="px-8 scroll-mt-20 flex flex-col gap-4 lg:grid-cols-2 lg:gap-8 lg:auto-cols-auto lg:grid">
+
       <Card title="Calidad" paragraph="Altos estándares de grabado, permitiéndole al cliente obtener una alta seguridad a su segundo bien más preciado, quedando en regla bajo la nueva normativa." color="#7ADB78" />
       
       <Card title="Experiencia" paragraph="Nos diferenciamos por ser los primeros en estandarizar el servicio, en cuanto a calidad y experiencia de visita de nuestros clientes" color="#FFC876" />
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,13 +2,12 @@
 import clsx from "clsx";
 import React from "react";
 import { motion } from "framer-motion";
-import { ButtonCtaHero } from "@/components/buttons/CtaHero";
 import { HeroPicture } from "./IconsImages/HeroImage";
-
+import { Button } from "@nextui-org/button";
 
 export const HeroSection = () => {
   return (
-    <section id="inicio" className="flex items-center text-center justify-center flex-col gap-16 px-8 pt-48">
+    <section id="inicio" className="flex items-center text-center justify-center flex-col gap-16 px-8 pt-48 xl:pt-72">
       <div className="flex flex-col gap-4">
         <div className="flex flex-col justify-center text-center items-center">
           <motion.p
@@ -18,11 +17,14 @@ export const HeroSection = () => {
               "text-[38px]",
               "md:text-5xl",
               "lg:text-6xl",
+              "xl:text-7xl",
+              "xl:w-[900px]",
               "text-gray-900",
               "text-center",
               "font-bold",
-              "leading-tight px-8 md:px-8",
+              "leading-tight px-[30px] md:px-8",
               "md:leading-tight",
+              "xl:leading-tight",
 
               /* Dark Mode */
               "dark:text-white"
@@ -30,20 +32,19 @@ export const HeroSection = () => {
           >
             Solución integral en seguridad automotriz
           </motion.p>
-          <p></p>
         </div>
 
-        <div className="flex items-center justify-center flex-col gap-6 ">
-          <motion.p
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            className="md:px-28 lg:px-60 text-center text-[16px] text-gray-800 dark:text-[#CAC6DD] leading-7"
-          >
+        <motion.div
+          initial={{ y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          className="flex items-center justify-center flex-col gap-6 "
+        >
+          <p className="md:px-28 lg:px-60 text-center text-[16px] text-gray-800 dark:text-[#CAC6DD] leading-7">
             Compromiso en otorgar soluciones integrales que protejan a nuestros clientes y sus vehículos.
-          </motion.p>
+          </p>
 
-          <ButtonCtaHero title={"Contactanos"} />
-        </div>
+          <Button className="bg-green-500 dark:bg-purple-500" size="lg">Contactanos</Button>
+        </motion.div>
       </div>
       <HeroPicture />
     </section>
