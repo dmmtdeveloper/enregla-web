67fa5cacf64fe4cf5cb5b86651696ad061d0f6b9
David Martínez <dmmtapia.ux@gmail.com>
2024-08-05 23:08:49 -0400

    changes

--- components/OurServices.tsx
+++ components/OurServices.tsx
@@ -1,53 +1,18 @@
-import React, { SVGProps } from "react";
-import styles, { layout } from "@/lib/style";
-import Image from "next/image";
-import radarImageWhite from "@/public/assets/img/radarappwhite.png";
-import radarImageBlack from "@/public/assets/img/radarappblack.png";
 import { MovingBorderDemo } from "./buttons/ButtonMoving";
 import { services } from "@/lib/OurServices";
 import clsx from "clsx";
-import { RippleSection } from "./RippleSection";
 import Ripple from "./magicui/ripple";
 import { Auto } from "./nav/Auto";
+import { OurServicesCard } from "./OurServices/CardServices";
 
-type OurServiceProps = {
-  title: string;
-  content: string;
-  index: number;
-  icon: React.ComponentType<SVGProps<SVGSVGElement>>;
-};
-
-const OurServicesCard: React.FC<OurServiceProps> = ({ icon: icon, title, content, index }) => {
-  return (
-    <div>
-      <div className="flex flex-col gap-2">
-        <h4 className={clsx("font-semibold", "dark:text-white", "text-black-100", "text-[24px]", "leading-[23px]")}>
-          {title}
-        </h4>
-        <p
-          className={clsx(
-            "dark:text-slate-400",
-            "text-black-100",
-            "text-[16px]",
-            "leading-[24px]",
-            "mb-1",
-            "xl:max-w-[600px]"
-          )}
-        >
-          {content}
-        </p>
-      </div>
-    </div>
-  );
-};
 
 export const OurServices = () => {
   return (
     <section
       id="nosotros"
-      className={`${layout.sectionReverse} dark:bg-black-100 flex-col-reverse scroll-mb-10 scroll-mt-16 xl:scroll-mt-0 xl:px-32`}
+      className={` flex items-center justify-center md:flex-row dark:bg-black-100 flex-col-reverse scroll-mb-10 scroll-mt-16 xl:scroll-mt-0 xl:px-8`}
     >
-      <div className="flex-1 md:mr-10 mr-0 md:mt-0 mt-10 relative flex justify-center items-center">
+      <div className="xl:w-[50%] md:w-[50%] w-[100%] relative flex justify-center items-center">
         <div className="relative flex h-[800px] w-full flex-col items-center justify-center overflow-hidden rounded-lg">
           <Auto />
           <Ripple />
