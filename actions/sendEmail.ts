d73209dab1dcdf83052a04c603110e754a5f01df
David Martínez <dmmtapia.ux@gmail.com>
2024-08-07 07:12:39 -0400

    changes

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,8 +1,18 @@
-"use server"
+"use server";
+import { Resend } from "resend";
 
-export const sendEmail = async (FormData:FormData) => {
-    console.log("Running on Server")
-    console.log(FormData.get("senderEmail"));
-    console.log(FormData.get("senderName"));
-    console.log(FormData.get("message"));
-}
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (FormData: FormData) => {
+  console.log("Running on Server");
+  console.log(FormData.get("senderEmail"));
+  console.log(FormData.get("senderName"));
+  console.log(FormData.get("message"));
+
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "dmmtapia.ux@gmail.com",
+    subject: "Message from contact form",
+    text: "Hello world",
+  });
+};
